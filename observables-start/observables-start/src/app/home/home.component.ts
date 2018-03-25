ded3902fc8edb75ebf92aa51812d84860ced6b55
Nadir Nasir <i@nadir.pk>
2018-03-25 16:14:45 +0500

    created a simple and slightly advanced observer

--- observables-start/observables-start/src/app/home/home.component.ts
+++ observables-start/observables-start/src/app/home/home.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx' ;
+import { Subscriber, Observer } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  mySubscriber: Subscriber<any>;
 
   constructor() { }
 
   ngOnInit() {
+    // const myObservable = Observable.interval(2000)
+    // myObservable.subscribe(
+    //   (number: Number) => {
+    //     console.log(number)
+    //   }
+    // );
+
+    const myObservable = Observable.create(
+      (observer: Observer<string>) => {
+        setTimeout(() => {
+          observer.next('First Packet of data');
+        }, 2000);
+        setTimeout(() => {
+          observer.next('Second Packet of data');
+        }, 4000);
+        setTimeout(() => {
+          observer.next('Third Packet of data');
+        }, 6000);
+        setTimeout(() => {
+          observer.error('This failed');
+        }, 8000);
+        setTimeout(() => {
+          observer.complete();
+        }, 10000);
+      }
+    );
+
+    this.mySubscriber = myObservable.subscribe(
+      (data: String) => {
+        console.log(data);
+      },
+      (data: String) => {
+        console.log("ERROR OCCURED: " + data);
+      },
+      () => {
+        console.log("Completed");
+      },
+    );
+
   }
 
+  ngOnDestroy() {
+    this.mySubscriber.unsubscribe();
+  }
 }
